0e982fea1629a8ae67331a121ec5be0687bcf26b
sophiamaedler <maedler@biochem.mpg.de>
2023-05-11 12:55:38 +0200

    move pretrained models and make sure they are packaged with the code

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    include_package_data=True,
 )
 
 if platform.system() == "Linux":
